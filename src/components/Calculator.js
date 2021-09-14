af046deca513dce9f1676658db8d455ddbd581fe
JAAR91 <jaarriagaramos@gmail.com>
2021-09-14 13:15:25 -0600

    add header, home, calculator and quote

---
+++ src/components/Calculator.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Calculator extends React.Component {
+  constructor() {
+    super();
+    this.list = [];
+  }
+
+  render() {
+    const value = (
+      <div className="Calculator row p-0 shadow fs-2 mx-auto mt-3 rounded-3">
+        <p className="col-12 fs-1 bg-secondary text-white m-0 text-end rounded-top" value="0">0</p>
+        <button className="col-3 p-5 bg-white" type="button">AC</button>
+        <button className="col-3 p-5 bg-white" type="button">+/-</button>
+        <button className="col-3 p-5 bg-white" type="button">%</button>
+        <button className="col-3 p-5 bg-orange" type="button">÷</button>
+        <button className="col-3 p-5 bg-white" type="button">7</button>
+        <button className="col-3 p-5 bg-white" type="button">8</button>
+        <button className="col-3 p-5 bg-white" type="button">9</button>
+        <button className="col-3 p-5 bg-orange" type="button">x</button>
+        <button className="col-3 p-5 bg-white" type="button">4</button>
+        <button className="col-3 p-5 bg-white" type="button">5</button>
+        <button className="col-3 p-5 bg-white" type="button">6</button>
+        <button className="col-3 p-5 bg-orange" type="button">-</button>
+        <button className="col-3 p-5 bg-white" type="button">1</button>
+        <button className="col-3 p-5 bg-white" type="button">2</button>
+        <button className="col-3 p-5 bg-white" type="button">3</button>
+        <button className="col-3 p-5 bg-orange" type="button">+</button>
+        <button className="col-6 p-5 bg-white" type="button">0</button>
+        <button className="col-3 p-5 bg-white" type="button">.</button>
+        <button className="col-3 p-5 bg-orange" type="button">=</button>
+      </div>
+    );
+    return value;
+  }
+}
+
+const myCalculator = () => {
+  ReactDOM.render(<Calculator />,
+    document.getElementById('container'));
+};
+
+export default myCalculator;
