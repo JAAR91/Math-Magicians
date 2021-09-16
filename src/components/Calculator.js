39afb7aa97977c74c5ff95c33242f55909991ce2
JAAR91 <jaarriagaramos@gmail.com>
2021-09-15 18:15:10 -0600

    add events to the components by passing then from the class to the child components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,18 +1,43 @@
-const CalcButton = (props) => {
-  let ans = <button className="col-3 p-5 bg-orange" type="button">{props.text}</button>;
-  if (props.Bclass === 'true') {
-    ans = <button className="col-3 p-5 bg-white" type="button">{props.text}</button>;
-  }
-  if (props.text === '0') {
-    ans = <button className="col-6 p-5 bg-white" type="button">{props.text}</button>;
-  }
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 
+export const CalcDisplay = (props) => {
+  let ans = <span className="CalcDisplay col-12 fs-1 bg-secondary text-white m-0 text-end rounded-top py-4">{props.total}</span>;
+  if (!props.total) {
+    ans = <span className="CalcDisplay col-12 fs-1 bg-secondary text-white m-0 text-end rounded-top py-4">{props.next}</span>;
+  }
   return ans;
 };
 
-export const CalcDisplay = (props) => {
-  const ans = <span className="col-12 fs-1 bg-secondary text-white m-0 text-end rounded-top py-4" value="0">{props.data}</span>;
-  return ans;
+class CalcButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const { cFunction } = this.props;
+    cFunction(e.target.textContent);
+  }
+
+  render() {
+    const { text, Bclass } = this.props;
+    let ans = <button className="col-3 p-5 bg-orange" type="button" onClick={this.handleClick}>{text}</button>;
+    if (Bclass === 'true') {
+      ans = <button className="col-3 p-5 bg-white" type="button" onClick={this.handleClick}>{text}</button>;
+    }
+    if (text === '0') {
+      ans = <button className="col-6 p-5 bg-white" type="button" onClick={this.handleClick}>{text}</button>;
+    }
+
+    return ans;
+  }
+}
+
+CalcButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  Bclass: PropTypes.string.isRequired,
+  cFunction: PropTypes.func.isRequired,
 };
 
 export default CalcButton;
