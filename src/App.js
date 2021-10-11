abca7def08ef1e0b33b97f7f281cb9eacc826e3f
JAAR91 <jaarriagaramos@gmail.com>
2021-10-11 11:28:12 -0600

    create navbar and routes for the diferent pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <p>ROCKETS</p>
+        </Route>
+        <Route exact path="/missions">
+          <p>MISSIONS</p>
+        </Route>
+        <Route exact path="/myprofile">
+          <p>My profile</p>
+        </Route>
+      </Switch>
+    </>
   );
 }
 
