abcce6561a304d181cee07a799f6838adbec92fc
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-10-12 12:44:25 +0100

    fetch rocket data and display

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,14 +1,35 @@
+/* eslint-disable no-console */
 /* eslint-disable react/destructuring-assignment */
-import React from 'react';
-import RocketItem from './RocketItem';
+import { React, useEffect } from 'react';
+import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import Rocket from './RocketItem';
+import { loadRocketsData } from '../redux/rockets/rockets';
 
-const RocketList = (rockets) => (
-  <ul className="allRockets">
-    {rockets.forEach((rocket) => {
-      const { id } = rocket;
-        <RocketItem key={id} rocket={rocket} />;
-    })}
-  </ul>
-);
+const RocketList = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  const loadRocketsAction = bindActionCreators(loadRocketsData, dispatch);
+
+  useEffect(() => {
+    loadRocketsAction();
+    return null;
+  }, []);
+
+  return (
+    <ul className="allRockets">
+      {/* {rockets.forEach((rocket) => {
+        const { id } = rocket;
+        console.log(rocket.rocket_name);
+          <li>
+            <Rocket key={id} rocket={rocket} />
+          </li>;
+      })} */}
+      {rockets.map((rocket) => (
+        <Rocket key={rocket.id} data={rocket} />
+      ))}
+    </ul>
+  );
+};
 
 export default RocketList;
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,10 +1,25 @@
-// action constants
+const LOAD_ROCKETS = 'SPACE_TRAVELERS/ROCKETS/LOAD_ROCKETS';
 
-// const initialState = []
-//  reducer
-const reducer = () => {};
-// actions
+const initialState = [];
 
-// Action creators
+export const loadRockets = (payload) => ({
+  type: LOAD_ROCKETS,
+  payload,
+});
+
+export const loadRocketsData = () => async (dispatch) => {
+  const res = await fetch('https://api.spacexdata.com/v3/rockets');
+  const data = await res.json();
+  // eslint-disable-next-line
+  console.log(data);
+  dispatch(loadRockets(data));
+};
+
+export const reducer = (state = initialState, action) => {
+  if (action.type === LOAD_ROCKETS) {
+    return action.payload;
+  }
+  return state;
+};
 
 export default reducer;
