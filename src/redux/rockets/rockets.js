9183b0d4f2b699e2f6c95ad6913aad18afa09845
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-10-12 21:08:27 +0100

    add conditional rendering of button

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,9 +1,21 @@
 const LOAD_ROCKETS = 'SPACE_TRAVELERS/ROCKETS/LOAD_ROCKETS';
+const RESERVE_ROCKET = 'SPACE_TRAVELERS/ROCKETS/RESERVE_ROCKETS';
+const CANCEL_ROCKET = 'SPACE_TRAVELERS/ROCKETS/CANCEL_ROCKETS';
 
 const initialState = [];
 
 export const loadRockets = (payload) => ({
   type: LOAD_ROCKETS,
+  payload,
+});
+
+export const reserveRockets = (payload) => ({
+  type: RESERVE_ROCKET,
+  payload,
+});
+
+export const cancelRockets = (payload) => ({
+  type: CANCEL_ROCKET,
   payload,
 });
 
@@ -16,10 +28,15 @@ export const loadRocketsData = () => async (dispatch) => {
 };
 
 export const reducer = (state = initialState, action) => {
-  if (action.type === LOAD_ROCKETS) {
-    return action.payload;
+  switch (action.type) {
+    case LOAD_ROCKETS:
+      return action.payload;
+    case RESERVE_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.payload) return rocket;
+        return { ...rocket, reserved: true };
+      });
+    default: return state;
   }
-  return state;
 };
-
 export default reducer;
